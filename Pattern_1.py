3bb4e34c7f0133093337a057ccc738bd89d2de7c
codewizard32 <131467398+codewizard32@users.noreply.github.com>
2023-05-11 14:36:36 +0530

    Create Pattern_1.py

---
+++ Pattern_1.py
@@ -0,0 +1,22 @@
+'''
+***********
+**       **
+* *     * *
+*  *   *  *
+*   * *   *
+*    ***  *
+*    ***  *
+*   *   * *
+*  *     **
+* *       *
+***********
+'''
+n = int(input("Enter an odd integer: "))
+for i in range(n):
+    for j in range(n):
+        # left diagonal, right diagonal, top horizontal, bottom horizontal, left vertical, right vertical
+        if i == j or i + j == n - 1 or i == 0 or i == n - 1 or j == 0 or j == n - 1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
